2861da569ff3638eb7512d4aa4d7986a510f1990
KageNoNeko <3940317+KageNoNeko@users.noreply.github.com>
2018-11-14 15:57:53 +0200

    image from url

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { parse } from 'query-string';
 import { Puzzle } from './Puzzle';
 
-const app = new Puzzle(
-    { rows: 3, columns: 3 },
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Inveraray_Castle_-_south-west_facade.jpg/1200px-Inveraray_Castle_-_south-west_facade.jpg',
-    1200, 738, { backgroundColor: 0x1099bb });
+const urlParams = parse(window.location.search);
+if (urlParams[ 'url' ] || urlParams[ 'path' ]) {
 
-document.body.appendChild(app.view);
+    const app = new Puzzle(
+        { rows: 3, columns: 3 },
+        // wikipedia/commons/thumb/b/bf/Inveraray_Castle_-_south-west_facade.jpg/1200px-Inveraray_Castle_-_south-west_facade.jpg
+        <string>urlParams[ 'url' ] || `https://upload.wikimedia.org/${urlParams[ 'path' ]}`,
+        1200, 738, { backgroundColor: 0x1099bb });
+
+    document.body.appendChild(app.view);
+} else {
+
+    document.body.innerHTML = 'No puzzle';
+}
