dbccb7882995ff47bc863b5077d25ff136c9ca91
KageNoNeko <3940317+KageNoNeko@users.noreply.github.com>
2018-11-16 18:39:42 +0200

    image fit, resize

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { parse } from 'query-string';
 import { Puzzle } from './Puzzle';
+import './images/steve.jpg';
 
 const urlParams = parse(window.location.search);
-if (urlParams[ 'url' ] || urlParams[ 'path' ]) {
+if (urlParams[ 'url' ] || urlParams[ 'path' ] || urlParams[ 'local' ]) {
 
     const app = new Puzzle(
         { rows: 3, columns: 3 },
-        // wikipedia/commons/thumb/b/bf/Inveraray_Castle_-_south-west_facade.jpg/1200px-Inveraray_Castle_-_south-west_facade.jpg
-        <string>urlParams[ 'url' ] || `https://upload.wikimedia.org/${urlParams[ 'path' ]}`,
-        1200, 738, { backgroundColor: 0x1099bb });
+        <string>urlParams[ 'url' ] || (urlParams[ 'path' ] ? `https://upload.wikimedia.org/${urlParams[ 'path' ]}` : `/images/${urlParams[ 'local' ]}`),
+        { backgroundColor: 0x1099bb, autoResize: true, resolution: devicePixelRatio });
 
     document.body.appendChild(app.view);
+
+    window.addEventListener('resize', () => app.toFullScreen());
+
+    app.toFullScreen();
 } else {
 
-    document.body.innerHTML = '<h5>No puzzle</h5><p>Try to add one of next URL params:</p><pre>?url=&lt;image URL&gt;</pre><pre>?path=&lt;image URL path on https://upload.wikimedia.org/&gt;</pre>';
+    document.body.innerHTML = '<h5>No puzzle</h5><p>Try to add one of next URL params:</p><pre>?url=&lt;image URL&gt;</pre><pre>?path=&lt;image URL path on https://upload.wikimedia.org/&gt;</pre><pre>?local=steve.jpg</pre>';
 }
